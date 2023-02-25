cd9bc680bef3734a38004542306e30fb37ea1e88
anbuganapathy <anbuganapathy.s@gmail.com>
2023-02-25 19:31:02 +0530

    JSONusingforofloop

---
+++ script.js
@@ -0,0 +1,15 @@
+let json = [{
+    "id" : "1", 
+    "msg"   : "hi",
+    "tid" : "2013-05-05 23:35",
+    "fromWho": "hello1@email.se"
+},
+{
+    "id" : "2", 
+    "msg"   : "there",
+    "tid" : "2013-05-05 23:45",
+    "fromWho": "hello2@email.se"
+}];
+for (var key of json) { 
+    console.log(key);
+}
