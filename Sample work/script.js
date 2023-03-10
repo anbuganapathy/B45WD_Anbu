1dae54eb7e332db14c401ef0347f8e56ab45b117
anbuganapathy <anbuganapathy.s@gmail.com>
2023-03-10 13:47:39 +0530

    updated links and tasks

--- Sample work/script.js
+++ Sample work/script.js
@@ -1,13 +1,33 @@
-//Remove duplicates from an array
+// let rotate = function(arr, numberOfShifts) {
+//   let deletedArray = arr.splice(arr.length-numberOfShifts);
+//   for (let i=0; i < deletedArray.length; i++){
+//       arr.splice(i,0,deletedArray[i]);
+//   }
 
-let arrD = [1, 9, 15, 1, 78, 3, 48];
+//   return arr;
+// };
 
-(function () {
-  for (let i = 0; i < arrD.length; i++) {
-      if (arrD[i] == arrD[i + 1]) {
-          console.log(arrD[i+1]);
-          arrD.pop(arrD[i+1]);
-      }
+// console.log(rotate([1, 2, 3, 4, 5, 6, 7], 1));
+
+// const str = 'i have learned something new today';
+// const arr = str.split(" ");
+// let func = ()=> {
+
+//     for (var i = 0; i < arr.length; i++) {
+//         arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+//     }
+//     console.log(arr.join(" "));
+// }
+// func();
+
+let arrRotate = (arr, n) => {
+  let arrshifted = [];
+  for (let i = 0; i < n; i++) {
+    arrshifted.push(arr[arr.length - n + i]);
   }
-  console.log(arrD);
-})();
+  for (let j = 0; j < arr.length - n; j++) {
+    arrshifted.push(arr[j]);
+  }
+  return arrshifted;
+};
+console.log(arrRotate([1, 2, 3, 4, 5, 6, 7], 3));
