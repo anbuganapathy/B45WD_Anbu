a6fa55977a1b0b5565990c46a35fee28944b3ce8
anbuganapathy <anbuganapathy.s@gmail.com>
2023-03-15 18:31:31 +0530

    updated task5 file and links file

--- Excercise/script.js
+++ Excercise/script.js
@@ -1,33 +1,22 @@
-// let rotate = function(arr, numberOfShifts) {
-//   let deletedArray = arr.splice(arr.length-numberOfShifts);
-//   for (let i=0; i < deletedArray.length; i++){
-//       arr.splice(i,0,deletedArray[i]);
-//   }
+let isPrime = true;
+let number = 0;
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+let primeFunc = (arr)=>{
+  for (let j = 0; j < arr.length; j++) {
+    number = arr[j];
+    for (let i = 2; i < number; i++) {
+      if (number % i == 0) {
+        isPrime = false;
+        break;
+      }
+      else {
+        isPrime = true;
+      }
+    }
 
-//   return arr;
-// };
-
-// console.log(rotate([1, 2, 3, 4, 5, 6, 7], 1));
-
-// const str = 'i have learned something new today';
-// const arr = str.split(" ");
-// let func = ()=> {
-
-//     for (var i = 0; i < arr.length; i++) {
-//         arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
-//     }
-//     console.log(arr.join(" "));
-// }
-// func();
-
-let arrRotate = (arr, n) => {
-  let arrshifted = [];
-  for (let i = 0; i < n; i++) {
-    arrshifted.push(arr[arr.length - n + i]);
+    if (isPrime) {
+      console.log(number);
+    }
   }
-  for (let j = 0; j < arr.length - n; j++) {
-    arrshifted.push(arr[j]);
-  }
-  return arrshifted;
-};
-console.log(arrRotate([1, 2, 3, 4, 5, 6, 7], 3));
+}
+primeFunc(arr);
