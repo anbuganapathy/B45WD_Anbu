66e941ef9a2282d68e52eca0ddbad20d8d215dc9
anbuganapathy <anbuganapathy.s@gmail.com>
2023-03-21 12:31:38 +0530

    renamed Exercise folder name and updated project description

---
+++ Projects/First Webcode - Makeup API/script.js
@@ -0,0 +1,12 @@
+let div = document.createElement("div");
+div.innerText = "Welcome to MakeUp Kart";
+document.body.append(div);
+let img = document.createElement("img");
+img.setAttribute("src", "https://assets.teenvogue.com/photos/62cd79bf9116dd22289750ed/16:9/w_2560%2Cc_limit/GettyImages-487770577.jpg");
+img.setAttribute("height", "1000");
+img.setAttribute("width", "1000");
+div.appendChild(img);
+
+let p = document.createElement("p");
+p.innerText = "Welcome to MAKEUP API";
+document.body.append(p);
