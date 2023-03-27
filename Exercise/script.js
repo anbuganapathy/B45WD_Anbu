e02921f24bcb79a5eba63cd4635bada035036205
anbuganapathy <anbuganapathy.s@gmail.com>
2023-03-27 15:21:01 +0530

    added react folder and other files

--- Exercise/script.js
+++ Exercise/script.js
@@ -1,16 +1,1 @@
-let xhr = new XMLHttpRequest();
-xhr.open("GET", "https://restcountries.com/v3.1/all");
-xhr.send();
-xhr.onload = function () {
-  if (xhr.status == 200) {
-    let users = JSON.parse(xhr.response);
-    for (let i of users) {
-        if (i.hasOwnProperty("currencies")) {
-            if(i.currencies.hasOwnProperty("USD"))
-            console.log(i);
-      } 
-    }
-  } else {
-    console.log(xhr.status);
-  }
-};
+ 
