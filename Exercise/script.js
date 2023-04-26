fe9fcdd1415859fc73fb03d799d45178594f9ab5
Anbu Ganapathy <anbuganapathy.s@gmail.com>
2023-04-26 20:03:20 +0530

    added codes for Node JS

--- Exercise/script.js
+++ Exercise/script.js
@@ -1,5 +1,35 @@
-function callMethod(e){
-document.createElement('p');
-document.getElementById().innerText="Hello World!";
-document.appendChild('p');
-}
+let fs=require("fs");
+// const quote="Live more, worry less";
+// fs.writeFile("./sample.html",quote,(err)=>{
+//     console.log("created a html file");
+// })
+
+// for(let i=2;i<=10;i++){
+//     fs.writeFile(`./backup/text-${i}.html`,quote,(err)=>{
+//         console.log("created a html file");
+//     })
+// }
+
+// const quote="Good day!";
+// const[, ,n]=process.argv;
+// for(let i=1;i<=n;i++){
+//     fs.writeFile(`./backup/note-${i}.html`,quote,(err)=>{
+//         console.log("created a html file");
+//     })
+// }
+
+// fs.readdir("./backup",(err,files)=>{
+//     console.log("All file names:",files.length);
+//     var len=files.length;
+// })
+
+// for(let i=1;i<=10;i++){
+// fs.unlink(`./backup/note-${i}.html`,(err)=>{
+// console.log("Successfully deleted.");
+// })
+// }
+
+fs.unlink("./backup",(err)=>{
+console.log("Successfully deleted.");
+})
+
